1f74d160a32304e210e7e1237a632a8bf31fd22a
nocturnalastro <micosta@redhat.com>
2024-05-01 12:53:42 +0100

    Added prefix to stress container using a macro, round tripped the yaml to fix some formatting issues, added labels

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import yaml
+import yaml # might be better to use ruamel in the future
 import argparse
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 from io import StringIO
 
-__DEBUG = True
-
 parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug", action="store_true", default=False)
 parser.add_argument("-i", "--input", type=Path)
 parser.add_argument("-o", "--output", type=Path)
 parser.add_argument(
@@ -19,7 +18,17 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-env = Environment(loader=FileSystemLoader(args.templates))
+__DEBUG = args.debug
+
+env = Environment(loader=FileSystemLoader(args.templates), keep_trailing_newline=False)
+def from_yaml(input, indent=2):
+    return yaml.load(input, Loader=yaml.SafeLoader)
+env.filters["from_yaml"] = from_yaml
+
+def to_yaml(input, indent=2):
+    return yaml.dump(input, indent=indent, Dumper=yaml.SafeDumper)
+env.filters["to_yaml"] = to_yaml
+
 
 if __DEBUG:
     print(env.list_templates())
@@ -32,15 +41,16 @@ with args.input.open() as f:
             copies = m.get("copies")
             for i in range(0, copies or 1):
                 results.append(
-                    env.get_template(f"{m['template']}.yaml.j2").render(index=i, **m)
+                    to_yaml(from_yaml(env.get_template(f"{m['template']}.yaml.j2").render(index=i, **m)), 2)
                 )
 
-output = ""
+output_list = []
 for r in results:
     r = r.strip()
     if not r.startswith("---"):
-        output += "\n---\n"
-    output += r + "\n"
+        output_list.append("---")
+    output_list.append(r)
+output = "\n".join(output_list)
 
 if args.output:
     with args.output.open("w") as f:
